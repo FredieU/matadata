499a8202c53c95fafe6af6ede2f4734e7e96a169
fredie <fredieunabia@gmail.com>
2020-03-01 06:32:52 +0000

    Add LinkedInBadge component, fix local server

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import LinkedInBadge from './components/LinkedInBadge';
 import './App.css';
 
 export default function App() {
@@ -7,22 +8,7 @@ export default function App() {
       <header className="App-header">
         <h4>Under construction...coming soon!</h4>
         <small>In the meantime, check out my social links.</small>
-        <div
-          class="LI-profile-badge"
-          data-version="v1"
-          data-size="large"
-          data-locale="en_US"
-          data-type="horizontal"
-          data-theme="light"
-          data-vanity="fredieunabia"
-        >
-          <a
-            class="LI-simple-link"
-            href="https://uk.linkedin.com/in/fredieunabia?trk=profile-badge"
-          >
-            Fredie Unabia
-          </a>
-        </div>
+        <LinkedInBadge bgColour="light" layout="horizontal" size="large" />
       </header>
     </div>
   );
